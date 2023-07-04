ebb8a12ded006b60391a722f42816fc1db7e96fe
ahmet <abayir41@gmail.com>
2023-07-04 19:10:43 +0300

    feat (Editor) : new editor for MapGEn and Map Seetings

---
+++ Assets/MapGen/Editor/MapGeneratorEditor.cs
@@ -0,0 +1,51 @@
+﻿using MapGen.Map;
+using UnityEditor;
+using UnityEngine;
+
+namespace MapGen.Editor
+{
+    [CustomEditor(typeof(MapGenerator))]
+    public class MapGeneratorEditor : UnityEditor.Editor {
+        
+        private MapGenerator _mapGenerator;
+        private UnityEditor.Editor _mapEditor;
+        private UnityEditor.Editor _noiseSettings;
+        private UnityEditor.Editor _randomSettings;
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            
+            if (GUILayout.Button("Generate Map"))
+            {
+                _mapGenerator.GenerateMap();
+            }
+            
+            DrawSettingsEditor(_mapGenerator.MapSettings, ref _mapGenerator.mapSettingsFoldout, ref _mapEditor);
+        }
+
+        private void DrawSettingsEditor(Object settings, ref bool foldout, ref UnityEditor.Editor cachedEditor)
+        {
+            if(settings == null) return;
+            
+            foldout = EditorGUILayout.InspectorTitlebar(foldout, settings);
+
+            if (!foldout) return;
+            
+            using var check = new EditorGUI.ChangeCheckScope();
+
+            CreateCachedEditor(settings, null, ref cachedEditor);
+            cachedEditor.OnInspectorGUI();
+
+            if (check.changed)
+            {
+                _mapGenerator.GenerateMapAuto();
+            }
+        }
+
+        private void OnEnable()
+        {
+            _mapGenerator = (MapGenerator) target;
+        }
+    }
+}
---
+++ Assets/MapGen/Editor/MapSettingsEditor.cs
@@ -0,0 +1,39 @@
+﻿using MapGen.Map;
+using UnityEditor;
+using UnityEngine;
+
+namespace MapGen.Editor
+{
+    [CustomEditor(typeof(MapSettings))]
+    public class MapSettingsEditor : UnityEditor.Editor
+    {
+        private MapSettings _mapSettings;
+        private UnityEditor.Editor _objectPlacementNoiseSettings;
+        private UnityEditor.Editor _groundNoiseSettings;
+        private UnityEditor.Editor _randomSettings;
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            DrawSettingsEditor(_mapSettings.ObjectPlacementNoise, ref _mapSettings.objectPlacementFoldout, ref _objectPlacementNoiseSettings);
+            DrawSettingsEditor(_mapSettings.GroundPlacementNoise, ref _mapSettings.groundNoiseFoldout, ref _groundNoiseSettings);
+            DrawSettingsEditor(_mapSettings.RandomSettings, ref _mapSettings.randomSettingsFoldout, ref _randomSettings);
+        }
+
+        private static void DrawSettingsEditor(Object settings, ref bool foldout, ref UnityEditor.Editor cachedEditor)
+        {
+            if(settings == null) return;
+            
+            foldout = EditorGUILayout.InspectorTitlebar(foldout, settings);
+
+            if (!foldout) return;
+            CreateCachedEditor(settings, null, ref cachedEditor);
+            cachedEditor.OnInspectorGUI();
+        }
+
+        private void OnEnable()
+        {
+            _mapSettings = (MapSettings) target;
+        }
+    }
+}
