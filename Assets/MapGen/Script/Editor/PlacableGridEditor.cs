7e23804343919203f4f081fb3de7543af7192e50
ahmet <abayir41@gmail.com>
2023-07-10 16:58:55 +0300

    feat (Placable System) : Editor Utilitis

---
+++ Assets/MapGen/Script/Editor/PlacableGridEditor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using MapGen.Map;
+using MapGen.Placables;
+using UnityEditor;
+using UnityEngine;
+
+namespace MapGen.Editor
+{
+    [CustomEditor(typeof(PlacableGrid))]
+    public class PlacableGridEditor : UnityEditor.Editor
+    {
+        protected virtual void OnEnable()
+        {
+            var placableGrid = (PlacableGrid) target;
+            placableGrid.DrawGizmo = true;
+            placableGrid.OnValidate();
+        }
+
+        protected virtual void OnDisable()
+        {
+            var placableGrid = (PlacableGrid) target;
+            placableGrid.DrawGizmo = false;
+            placableGrid.OnValidate();
+        }
+    }
+    
+    [CustomEditor(typeof(PlacableNewGroundGrid))]
+    public class PlacableNewGroundEditor : PlacableGridEditor
+    {
+        
+    }
+    
+    [CustomEditor(typeof(PlacableNormalGrid))]
+    public class PlacableNormalEditor : PlacableGridEditor
+    {
+        
+    }
+    
+    [CustomEditor(typeof(PlacableShouldPlaceOnGroundGrid))]
+    public class PlacableShouldPlaceOnGroundEditor : PlacableGridEditor
+    {
+        
+    }
+    
+    
+
+}
