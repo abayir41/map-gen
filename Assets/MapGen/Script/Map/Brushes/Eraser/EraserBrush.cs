b5c022f3650c47eaaaff14baceba47f9ac0e3358
ahmet <abayir41@gmail.com>
2023-07-14 19:01:09 +0300

    feat (Brushes) : New Brushes

---
+++ Assets/MapGen/Script/Map/Brushes/Eraser/EraserBrush.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using MapGen.Placables;
+using UnityEngine;
+using Grid = MapGen.GridSystem.Grid;
+
+namespace MapGen.Map.Brushes.Eraser
+{
+    
+    [CreateAssetMenu(fileName = "Eraser Brush", menuName = "MapGen/Brushes/Eraser", order = 0)]
+    public class EraserBrush : Brush
+    {
+        public override List<Placable> Paint(List<Vector3Int> selectedCells, Grid grid)
+        {
+            foreach (var selectedCell in selectedCells)
+            {
+                if (grid.IsCellExist(selectedCell, out var cell) && cell.Item != null)
+                {
+                    WorldCreator.Instance.DestroyItem(cell.Item);
+                }
+            }
+
+            return new List<Placable>();
+        }
+    }
+}
---
+++ Assets/MapGen/Script/Map/Brushes/GroundBrush/GroundBrush.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using MapGen.GridSystem;
+using MapGen.Map.Brushes.NormalMap;
+using MapGen.Placables;
+using UnityEngine;
+using Weaver;
+using Grid = MapGen.GridSystem.Grid;
+
+namespace MapGen.Map.Brushes.GroundBrush
+{
+    
+    [CreateAssetMenu(fileName = "Ground Brush", menuName = "MapGen/Brushes/Ground/Ground Brush", order = 0)]
+    public class GroundBrush : Brush
+    {
+        [SerializeField] private GroundBrushSettings _groundBrushSettings;
+        public GroundBrushSettings GroundBrushSettings => _groundBrushSettings;
+
+        public override List<Placable> Paint(List<Vector3Int> selectedCells, Grid grid)
+        {
+            return CreateGround(selectedCells, grid);
+        }
+        
+        [MethodTimer]
+        private List<Placable> CreateGround(List<Vector3Int> selectedCells, Grid grid)
+        {
+            var result = new List<Placable>();
+            foreach (var selectedCell in selectedCells)
+            {
+                if (grid.IsCellExist(selectedCell, out var cell))
+                {
+                    cell.MakeCellCanBeFilledGround();
+                }
+
+                var placable = WorldCreator.Instance.SpawnObject(selectedCell, _groundBrushSettings.Ground,
+                    _groundBrushSettings.CellLayer, GroundBrushSettings.GROUND_ROTATION);
+                result.Add(placable);
+            }
+
+            return result;
+        }
+    }
+    
+}
