021806d6841e475112700ed0661cf7cb647dbe62
ahmet <abayir41@gmail.com>
2023-08-11 12:12:52 +0300

    +feat (Single Cell Brush): single cell brush added

---
+++ Assets/MapGen/Script/Map/Brushes/BrushAreas/SingleCellBrushArea.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace MapGen.Map.Brushes.BrushAreas
+{
+    [CreateAssetMenu(fileName = "Single Cell", menuName = "MapGen/Brush Areas/Single Cell", order = 0)]
+    public class SingleCellBrushArea : BrushArea
+    {
+        public override List<Vector3Int> GetBrushArea(Vector3Int startPoint)
+        {
+            return new List<Vector3Int>() { startPoint };
+        }
+    }
+}
